599dc0cab3866fa1a8cf616de43757d1e68d2964
Peter Adetola <peteradetola@gmail.com>
2022-10-17 12:54:53 -0700

    Print comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - point of entry
+ *
+ * Description: Possible combination of single digit numbers
+ *
+ * Return: Always 0
+ */
+
+int main(0)
+{
+	int = n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	putchar('\n');
+	return (0);
+}
+}
